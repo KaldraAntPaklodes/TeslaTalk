505920e00cae42852afced477bcf1f575ea2fd9c
KaldraAntPaklodes <bliudziust@gmail.com>
2023-07-04 18:34:17 +0300

    App

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 8080;
+
+const URI = process.env.DB_CONNECTION_STRING;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const client = new MongoClient(URI);
+
+app.get('/users', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con
+      .db('teslatalk')
+      .collection('users')
+      .find()
+      .toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const user = req.body;
+    const con = await client.connect();
+    const data = await con
+      .db('teslatalk')
+      .collection('users')
+      .insertOne(user);
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on the ${port} port`);
+});
---
+++ teslatalk/src/layouts/LoginLayout.jsx
@@ -0,0 +1,10 @@
+const LoginLayout = ({ children }) => {
+  return (
+    <div className="login-container">
+      <h1>Hello to my app!</h1>
+      {children}
+    </div>
+  );
+};
+
+export default LoginLayout;
